d5d46db585abbabd1534c5001fe53490d29ea402
folaareago <folajanded@yahoo.com>
2024-07-13 17:34:07 +0100

    swapped images

--- src/components/teams/teamsComponent.js
+++ src/components/teams/teamsComponent.js
@@ -6,9 +6,9 @@ function TeamsComponent() {
 
 const images = [
         { src: "/assets/images/teams/t-1.jpg", alt: "Team 1", name: "Yomi Shokan", role: "Ceo & Founder" },
-        { src: "/assets/images/teams/t-2.jpg", alt: "Team 2", name: "Benedicta Kodjo", role: "Executive Director", DesRole: "Assets Management and Client Relations" },
+        { src: "/assets/images/teams/t-4.jpg", alt: "Team 2", name: "Benedicta Kodjo", role: "Executive Director", DesRole: "Assets Management and Client Relations" },
         { src: "/assets/images/teams/t-3.jpg", alt: "Team 3", name: "Egnr Toba Adeyemi", role: "Executive Director", DesRole: "Project and Acquisition" },
-        { src: "/assets/images/teams/t-4.jpg", alt: "Team 4", name: "Folake Ashiru", role: "Chief Compliance Director" },
+        { src: "/assets/images/teams/t-2.jpg", alt: "Team 4", name: "Folake Ashiru", role: "Chief Compliance Director" },
         // { src: "/assets/images/teams/teams.png", alt: "Team 5", name: "Jayden Lee", role: "Frontend Developer" },
         // { src: "/assets/images/teams/teams.png", alt: "Team 6", name: "Graphics OJ", role: "Graphic Designer" }
       ];
