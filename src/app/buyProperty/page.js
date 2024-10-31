ed90738fd3d8bcba3f8825524f5c7f91e51deb9a
Kasali Gabriel <heayarnu@gmail.com>
2024-10-31 14:57:41 +0100

    Corrections made: 	modified:   src/app/buildProperty/page.js 	modified:   src/app/buyLand/page.js 	modified:   src/app/buyProperty/page.js 	modified:   src/components/footer/footerComponent.js 	modified:   src/components/getStarted/getStartedData.js 	modified:   src/components/services/servicesComponent.js 	modified:   src/components/teams/teamsComponent.js

--- src/app/buyProperty/page.js
+++ src/app/buyProperty/page.js
@@ -4,21 +4,24 @@ import FooterComponent from '@/components/footer/footerComponent';
 import Items from '@/components/items/items';
 import NavMain from '@/components/navigationBar/navMain';
 import NavbarSmallScreen from '@/components/navigationBar/navSmall';
+import { Suspense } from 'react';
 
 const page = () => {
 	return (
-		<div className="relative ">
-			<div className="bg-primary h-[6.5rem] w-full flex items-center justify-center">
-				<NavMain />
-				<NavbarSmallScreen />
-			</div>
+		<Suspense>
+			<div className="relative ">
+				<div className="bg-primary h-[6.5rem] w-full flex items-center justify-center">
+					<NavMain />
+					<NavbarSmallScreen />
+				</div>
 
-			<Items heading="Buy A Property" />
+				<Items heading="Buy A Property" />
 
-			<div className="xl:px-[6.3rem] max-xl:w-[90%] mx-auto">
-				<FooterComponent />
+				<div className="xl:px-[6.3rem] max-xl:w-[90%] mx-auto">
+					<FooterComponent />
+				</div>
 			</div>
-		</div>
+		</Suspense>
 	);
 };
 
