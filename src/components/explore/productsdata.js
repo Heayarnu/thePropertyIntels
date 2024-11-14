c143509b52d5e68a4d97247da286eda366a90d14
folaareago <folajanded@yahoo.com>
2024-11-14 18:04:12 +0100

    edit changes

--- src/components/explore/productsdata.js
+++ src/components/explore/productsdata.js
@@ -8,13 +8,13 @@ export const productGrid = [
 		'BUILD A PROPERTY',
 		'/buildProperty',
 	],
-	[
-		'forth',
-		'/assets/images/mookimg/ikoyi3.png',
-		'Buy a Property',
-		'/buyProperty',
-	],
-	// ['forth', '/assets/cr2.jpg', 'OWN A LAND', '/buyLand'],
+	// [
+	// 	'forth',
+	// 	'/assets/images/mookimg/ikoyi3.png',
+	// 	'Buy a Property',
+	// 	'/buyProperty',
+	// ],
+	 ['forth', '/assets/cr2.jpg', 'OWN A LAND', '/buyLand'],
 	// ["fifth", "/assets/images/mookimg/ikoyi5.png", "ikoyi5"],
 	// ["sixth", "/assets/images/mookimg/ikoyi1.png", "ikoyi1"],
 ];
